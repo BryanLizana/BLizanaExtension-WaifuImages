fc2c939d6f6506ffc208c573fb1554ae59233123
BryanLizana <bryandelao5@gmail.com>
2019-09-04 14:36:03 -0500

    L@s desgraciad@s de VSCode cambiaron el name del file js y css xd pero ya se arregló :)

--- out/vscodePath.js
+++ out/vscodePath.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const path = require("path");
 const base = path.dirname(require.main.filename);
-const cssPath = path.join(base, 'vs', 'workbench', 'workbench.main.css');
-const jsPath = path.join(base, 'vs', 'workbench', 'workbench.main.js');
+const cssPath = path.join(base, 'vs', 'workbench', 'workbench.desktop.main.css');
+const jsPath = path.join(base, 'vs', 'workbench', 'workbench.desktop.main.js');
 const cssPathBK = path.join(base, 'vs', 'workbench', 'workbench.main.css.backup');
 const jsPathBK = path.join(base, 'vs', 'workbench', 'workbench.main.js.backup');
 exports.default = {
